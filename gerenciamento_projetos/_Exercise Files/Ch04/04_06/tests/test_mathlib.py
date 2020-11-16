92e67a53350eb85b64eabd5646638fbe7a28340d
Caesar C. Cesar <caesarcesar.rs@gmail.com>
2020-11-16 15:15:53 -0300

    inclusão dos códigos de cursos do linkedin

--- gerenciamento_projetos/_Exercise Files/Ch04/04_06/tests/test_mathlib.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-import mathlib
-
-
-def test_neg():
-    with pytest.raises(ValueError):
-        mathlib.sqrt(-2)
--- gerenciamento_projetos/_Exercise Files/Ch04/challenge/tests/sqrt_cases.jl
+++
@@ -1,4 +0,0 @@
-{ "value": 1, "out": 1, "error": false}
-{ "value": 2, "out": 1.4142, "error": false}
-{ "value": 4, "out": 2, "error": false}
-{ "value": -2, "out": 0, "error": true}
